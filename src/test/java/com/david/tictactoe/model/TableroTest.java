a21f3250d58803c4182df12d3eb16ae63db8c66a
Mariano Navas <marianudo@gmail.com>
2023-11-26 11:21:19 +0100

    Reducir boilerplate

--- src/test/java/com/david/tictactoe/model/TableroTest.java
+++ src/test/java/com/david/tictactoe/model/TableroTest.java
@@ -2,6 +2,7 @@ package com.david.tictactoe.model;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static com.david.tictactoe.model.Casilla.*; // Reducir boilerplate code
 
 public class TableroTest {
     private Tablero tableroInicial = new Tablero();
@@ -9,15 +10,15 @@ public class TableroTest {
     // Happy path
     @Test public void testObtenerFichaDeCasillaVacia() {
         Casilla casilla = tableroInicial.getFicha(0, 0);
-        assertEquals(Casilla.VACIA, casilla);
+        assertEquals(VACIA, casilla);
     }
 
     @Test public void testObtenerFichaDeCasillaLlena() throws MovimientoNoValidoException {
         int fila = 0;
         int columna = 0;
 
-        tableroInicial.colocarFicha(Casilla.JUGADOR1, fila, columna);
-        assertNotEquals(Casilla.VACIA, tableroInicial.getFicha(fila, columna));
+        tableroInicial.colocarFicha(JUGADOR1, fila, columna);
+        assertNotEquals(VACIA, tableroInicial.getFicha(fila, columna));
     }
 
     // Errores
@@ -25,29 +26,31 @@ public class TableroTest {
     public void posicionarFichaEnFilaFueraDelTablero() throws MovimientoNoValidoException {
         int fila = 3;
         int columna = 0;
-        tableroInicial.colocarFicha(Casilla.JUGADOR1, fila, columna);
+        tableroInicial.colocarFicha(JUGADOR1, fila, columna);
     }
 
     @Test(expected = MovimientoNoValidoException.class)
     public void posicionarFichaEnColumnaFueraDelTablero() throws MovimientoNoValidoException {
         int fila = 0;
         int columna = 3;
-        tableroInicial.colocarFicha(Casilla.JUGADOR1, fila, columna);
+        tableroInicial.colocarFicha(JUGADOR1, fila, columna);
     }
 
     @Test(expected = MovimientoNoValidoException.class)
     public void posicionarFichaEnCasillaOcupadaPorMi() throws MovimientoNoValidoException {
         int filaJ1 = 0;
         int colJ1 = 0;
-        tableroInicial.colocarFicha(Casilla.JUGADOR1, filaJ1, colJ1);
-        tableroInicial.colocarFicha(Casilla.JUGADOR1, filaJ1, colJ1);
+        tableroInicial.colocarFicha(JUGADOR1, filaJ1, colJ1);
+        tableroInicial.colocarFicha(JUGADOR1, filaJ1, colJ1);
     }
 
     @Test(expected = MovimientoNoValidoException.class)
     public void posicionarFichaEnCasillaOcupadaPorOtro() throws MovimientoNoValidoException {
         int filaJ1 = 0;
         int colJ1 = 0;
-        tableroInicial.colocarFicha(Casilla.JUGADOR2, filaJ1, colJ1);
-        tableroInicial.colocarFicha(Casilla.JUGADOR1, filaJ1, colJ1);
+        tableroInicial.colocarFicha(JUGADOR2, filaJ1, colJ1);
+        tableroInicial.colocarFicha(JUGADOR1, filaJ1, colJ1);
+    }
+
     }
 }
